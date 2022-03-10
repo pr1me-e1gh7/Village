cbcb176f56b07d1260411c43f2a30b479dd5ced0
E-Albert <magicspoon123@gmail.com>
2022-03-10 14:32:20 -0500

    Added footer component

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ import Nav from './components/Nav';
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
+import Footer from './components/Footer';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -75,6 +76,7 @@ function App() {
               />
             </Routes>
           </StoreProvider>
+          <Footer/>
         </div>
       </Router>
     </ApolloProvider>
