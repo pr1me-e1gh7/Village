31bd43726f7ec96f8fb4aa4e50723e70c0617c3c
randerson321 <kuminachi121@gmail.com>
2022-03-15 10:36:20 -0400

    updated modals

---
+++ client/src/components/Modals/PreviousEntry.js
@@ -0,0 +1,38 @@
+export default function previousEntry() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+    // What goes here?
+    // const handleSubmit = () => "";
+
+  
+    return (
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Close
+        </Button>
+  
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Previous Entry</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p> Date Added: </p>
+            <text class="text-area">Notes</text>
+          </Modal.Body>
+          <Modal.Footer>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+  
+  render(<previousEntry />);
