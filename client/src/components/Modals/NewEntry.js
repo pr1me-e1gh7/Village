2457c6466ac3f528a912f6b49c30de007183125d
randerson321 <kuminachi121@gmail.com>
2022-03-17 08:53:34 -0400

    update newEntry modal

---
+++ client/src/components/Modals/NewEntry.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
+import Auth from '../../utils/auth';
+
+
+export default function newEntry() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+    // What goes here?
+    // const handleSubmit = () => "";
+
+  
+    return (
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Close
+        </Button>
+  
+        <Modal
+          show={show}
+          onHide={handleClose}
+          backdrop="static"
+          keyboard={false}
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Comment</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p> What would you like to say?: </p>
+            <input type="text"> </input>
+          </Modal.Body>
+          <Modal.Footer>
+              <Button class="submitComment"> Submit Comment</Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+  
+  render(<newEntry />);
